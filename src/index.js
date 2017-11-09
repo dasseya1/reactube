22f819ece2f202a1c0b6aa3131b1ef5634338a4e
Damien Asseya <dasseya1@Damiens-iMac.local>
2017-11-08 20:25:59 -0500

    Handling user input

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
+import SearchBar from './components/search_bar';
+
+<Searchbar />
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
