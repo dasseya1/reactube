5867aa3a2b7d038e626add467e056e5b7e654c89
Damien Asseya <etudescanada01@gmail.com>
2017-11-08 21:34:13 -0500

    Create a stateless component App

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import SearchBar from './search_bar';
+
+const App = () => (
+    <div>
+        <SearchBar />
+    </div>
+      
+  );
+
+  export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
-import SearchBar from './components/search_bar';
+import App from './components/app';
 
 const API_KEY = 'AIzaSyAuQCVeNfKhtRk9KlChQPT1n027DP0_5Ss';
-
-const App = () => (
-  <div>
-      <SearchBar />
-  </div>
-    
-);
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
