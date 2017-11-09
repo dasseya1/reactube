039b8ffeb878a180b0e3f71a417cf8337a7baba5
Damien Asseya <etudescanada01@gmail.com>
2017-11-08 20:43:14 -0500

    Created stateless component App

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import SearchBar from './components/search_bar';
 
-<Searchbar />
+const API_KEY = 'AIzaSyAuQCVeNfKhtRk9KlChQPT1n027DP0_5Ss';
+
+const App = () => (
+  <div>
+      <SearchBar />
+  </div>
+    
+);
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
