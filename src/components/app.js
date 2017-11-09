c18968cc56195874088000242f7146d75d9617b8
Damien Asseya <etudescanada01@gmail.com>
2017-11-08 22:40:12 -0500

    Refactored functional components to class components

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import SearchBar from './search_bar';
+import YTSearch from 'youtube-api-search';
 
-const App = () => (
-    <div>
-        <SearchBar />
-    </div>
-      
-  );
+const API_KEY = 'AIzaSyD3w0Ug3YJ05OoO6sBuwtNoEt8qMEYjc1E';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'Celine Dion'}, (videos) => {
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
+            <SearchBar />
+        );
+    }
+}
 
   export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import App from './components/app';
 
-const API_KEY = 'AIzaSyAuQCVeNfKhtRk9KlChQPT1n027DP0_5Ss';
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
