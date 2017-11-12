add4c02ef0cc9430ab84e9d2cc774af630ef09da
Damien Asseya <etudescanada01@gmail.com>
2017-11-12 06:13:18 -0500

    Completed throttling search term input

--- src/components/app.js
+++ src/components/app.js
@@ -3,6 +3,7 @@ import SearchBar from './search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './video_list';
 import VideoDetail  from './video_detail';
+import _ from 'lodash';
 
 const API_KEY = 'AIzaSyD3w0Ug3YJ05OoO6sBuwtNoEt8qMEYjc1E';
 
@@ -25,9 +26,10 @@ class App extends Component {
         });
     }
     render() {
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
         return (
             <div>
-                <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+                <SearchBar onSearchTermChange={videoSearch} />
                 <VideoDetail video={this.state.selectedVideo}/>
                 <VideoList
                     onVideoSelect={selectVideo => this.setState({selectVideo})}
